c318e7bb56d2d329b81b37a02bf5409e86083249
Caio M D <caio.martins176@gmail.com>
2024-05-22 22:43:38 -0300

    chore: create a static method

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -8,8 +8,8 @@ export class Entity<T> {
     return this._id
   }
 
-  protected constructor(props: any, id?: string) {
+  protected constructor(props: any, id?: UniqueEntityID) {
     this.props = props
-    this._id = new UniqueEntityID(id)
+    this._id = id ?? new UniqueEntityID()
   }
 }
